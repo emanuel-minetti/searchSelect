ab532cc0532e3bdb1ebe1e0e166eee55355b80a8
Emanuel Minetti <e.minetti@posteo.de>
2022-09-04 16:43:14 +0200

    Refactored to be useful.

--- js/searchSelect.js
+++ js/searchSelect.js
@@ -1,46 +1,57 @@
 window.onload = () => {
-    const selectDiv = document.getElementById('selectDiv');
-    makeSearchable(selectDiv, dropdownItems);
+    const selectDivs = document.getElementsByClassName('search-select-div');
+    Array.from(selectDivs).forEach(div => makeSearchable(div));
 };
 
-function makeSearchable(selectDiv, dropdownItems) {
-    const selectInputElm = selectDiv.getElementsByClassName('search-select-name').item(0);
-    const selectInputValueElm = selectDiv.getElementsByClassName('search-select-value').item(0);
-    const dropdownUl = selectDiv.getElementsByTagName('ul').item(0);
-    selectInputElm.value = '';
-    selectInputElm.addEventListener('click', () => {
+function makeSearchable(selectDiv) {
+    const selectElem = selectDiv.getElementsByTagName('select').item(0);
+    selectElem.hidden = true;
+    const inputTextElm = document.createElement('input');
+    inputTextElm.type = 'text';
+    inputTextElm.classList.add('form-control');
+    inputTextElm.autocomplete = 'off';
+    selectDiv.insertAdjacentElement('beforeend', inputTextElm);
+    const inputHiddenElem = document.createElement('input');
+    inputHiddenElem.type = 'hidden';
+    inputHiddenElem.name = selectElem.name;
+    selectDiv.insertAdjacentElement('beforeend', inputHiddenElem);
+    const dropdownDiv = document.createElement('div');
+    dropdownDiv.classList.add('dropdown');
+    const dropdownUl = document.createElement('ul');
+    dropdownUl.classList.add('dropdown-menu');
+    dropdownDiv.insertAdjacentElement('beforeend', dropdownUl);
+    selectDiv.insertAdjacentElement('beforeend', dropdownDiv);
+    inputTextElm.value = '';
+    inputTextElm.addEventListener('click', () => {
         dropdownUl.classList.contains('show') ?
             dropdownUl.classList.remove('show') :
             dropdownUl.classList.add('show');
     });
     const dropdownLiArray = [];
-    dropdownItems.forEach(item => {
-        const liElm = document.createElement('li');
-        liElm.id = item.id;
-        liElm.dataset.value = item.value;
-        liElm.innerText = item.name;
-        liElm.classList.add('dropdown-item');
-        dropdownUl.insertAdjacentElement('beforeend', liElm);
-        dropdownLiArray.push(liElm);
-    });
-    dropdownLiArray.forEach(liElm => {
-       liElm.addEventListener('click', () => {
-           selectInputElm.value = liElm.innerText;
-           selectInputValueElm.value = liElm.dataset.value;
-           dropdownUl.classList.remove('show');
-       });
+    Array.from(selectElem.options).forEach(option => {
+        const liElem = document.createElement('li');
+        liElem.id = selectElem.id + option.value;
+        liElem.innerText = option.innerText;
+        liElem.classList.add('dropdown-item');
+        dropdownUl.insertAdjacentElement('beforeend', liElem);
+        liElem.addEventListener('click', () => {
+            inputTextElm.value = liElem.innerText;
+            inputHiddenElem.value = option.value;
+            dropdownUl.classList.remove('show');
+        });
+        dropdownLiArray.push(liElem);
     });
     document.addEventListener('click', evt => {
-        if (!dropdownLiArray.find(liElm => liElm === evt.target || selectInputElm === evt.target)) {
+        if (!dropdownLiArray.find(liElm => liElm === evt.target || inputTextElm === evt.target)) {
             dropdownUl.classList.remove('show');
         }
     });
     const liCollection = dropdownUl.getElementsByTagName('li');
-    selectInputElm.addEventListener('input', () => {
-        const removedItems = dropdownLiArray.filter(liElm =>
-            !liElm.innerText.toLowerCase().includes(selectInputElm.value.toLowerCase())
-        );
+    inputTextElm.addEventListener('input', () => {
         dropdownLiArray.forEach(item => item.hidden = false);
-        removedItems.forEach(item => liCollection.namedItem(item.id).hidden = true);
+        dropdownLiArray.filter(liElm =>
+            !liElm.innerText.toLowerCase().includes(inputTextElm.value.toLowerCase())
+        ).forEach(item => liCollection.namedItem(item.id).hidden = true);
     });
+    selectElem.remove();
 }
